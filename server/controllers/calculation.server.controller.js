5c070291e1d298718d6df2dbfb08b8923b173453
lgdelacruz <bboyjacks@gmail.com>
2019-11-10 17:41:46 -0800

    Make some cross origin changes

--- server/controllers/calculation.server.controller.js
+++ server/controllers/calculation.server.controller.js
@@ -1,31 +1,53 @@
-const engine = require('./calculation.engine.controller');
+const engine = require("./calculation.engine.controller");
 
 exports.calculateGeneralPlume = function(req, res) {
-    var delta = 1;
-    const maxX = 10000;
-    var x = 0, y = 0, z = 0;
-    var data = [];
-    var material = 10;
-    for (x; x <= maxX; x += delta) {
-        data.push({
-            x: x,
-            y: engine.engine.gaussian("F", x, y, z, req.body.receptorHeights, req.body.windSpeed, material)
-        });
-    }
-    res.send(data);
-}
+  var delta = 1;
+  const maxX = 10000;
+  var x = 0,
+    y = 0,
+    z = 0;
+  var data = [];
+  var material = Math.pow(10, 9);
+  for (x; x <= maxX; x += delta) {
+    data.push({
+      x: x,
+      y: engine.engine.gaussian(
+        "F",
+        x,
+        y,
+        z,
+        req.body.receptorHeights,
+        req.body.windSpeed,
+        material
+      )
+    });
+  }
+  res.header("Access-Control-Allow-Origin", "*");
+  res.send(data);
+};
 
 exports.calculateFire = function(req, res) {
-    var delta = 1;
-    const maxX = 10000;
-    var x = 0, y = 0, z = 0;
-    var data = [];
-    var material = 10;
-    for (x; x <= maxX; x += delta) {
-        data.push({
-            x: x,
-            y: engine.engine.gaussian(req.body.stability.toUpperCase(), x, y, z, req.body.receptorHeights, req.body.windSpeed, material)
-        });
-    }
-    res.send(data);
-}
+  var delta = 1;
+  const maxX = 10000;
+  var x = 0,
+    y = 0,
+    z = 0;
+  var data = [];
+  var material = Math.pow(10, 9);
+  for (x; x <= maxX; x += delta) {
+    data.push({
+      x: x,
+      y: engine.engine.gaussian(
+        req.body.stability.toUpperCase(),
+        x,
+        y,
+        z,
+        req.body.receptorHeights,
+        req.body.windSpeed,
+        material
+      )
+    });
+  }
+  res.header("Access-Control-Allow-Origin", "*");
+  res.send(data);
+};
--- server/routes/calculation.server.routes.js
+++ server/routes/calculation.server.routes.js
@@ -1,11 +1,31 @@
-var express = require('express'),
-    controller = require('../controllers/calculation.server.controller.js'),
-    router = express.Router();
+var express = require("express"),
+  controller = require("../controllers/calculation.server.controller.js"),
+  router = express.Router();
 
-router.route('/fire')
-    .post(controller.calculateFire);
+router
+  .route("/fire")
+  .post(controller.calculateFire)
+  .options((req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, Accept"
+    );
+    res.send();
+  });
 
-router.route('/plume')
-    .post(controller.calculateGeneralPlume);
+router
+  .route("/plume")
+  .post(controller.calculateGeneralPlume)
+  .options((req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, Accept"
+    );
+    res.send();
+  });
 
 module.exports = router;
