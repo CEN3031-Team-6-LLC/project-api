d08631965aeb8da344ff55f8053839361ff0c432
amakUfl <a.makarevitch@ufl.edu>
2019-11-12 21:08:06 -0500

    Fixing typo

--- server/config/cors.handler.js
+++ server/config/cors.handler.js
@@ -5,7 +5,7 @@ const config = process.env.NODE_ENV == "PROD" ? {} : require("./config.js");
 exports.CORS_handshake = function(req, res) {
     res.header("Access-Control-Allow-Origin", process.env.ALLOWED_UI_DOMAINS || config.allowed_ui_domains);
     res.header("Access-Control-Allow-Methods", "POST");
-    response.header("Access-Control-Allow-Headers", 
+    res.header("Access-Control-Allow-Headers", 
         "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin");
     // res.header(
     //   "Access-Control-Allow-Headers",
