da39157c800e4360f18147f21482577b61f39fdb
lgdelacruz <bboyjacks@gmail.com>
2019-11-14 18:52:26 -0800

    Fixed cors issue

--- server.js
+++ server.js
@@ -1,35 +1,45 @@
 // var server = app.start();
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
 // uncomment for db access
 // const mongoose = require('mongoose');
-// const config = require('./server/config/config');
-
 
 if (cluster.isMaster) {
-    console.log(`Master ${process.pid} is running in ${process.env.NODE_ENV || "LOCAL"} environmet.`);
-    const config = process.env.NODE_ENV == "PROD" ? {} : require('./server/config/config');
-    console.log("The following clients are allowed to usi this API: ", process.env.ALLOWED_UI_DOMAINS || config.allowed_ui_domains);
+  console.log(
+    `Master ${process.pid} is running in ${process.env.NODE_ENV ||
+      "LOCAL"} environmet.`
+  );
+  const config =
+    process.env.NODE_ENV == "PROD" ? {} : require("./server/config/config");
+  console.log(
+    "The following clients are allowed to usi this API: ",
+    process.env.ALLOWED_UI_DOMAINS || config.allowed_ui_domains
+  );
+  console.log(`Master ${process.pid} is running`);
+  console.log(
+    `Master ${process.pid} is running in ${process.env.NODE_ENV ||
+      "LOCAL"} environmet.`
+  );
 
-    // workers to process clients' requests
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
+  // workers to process clients' requests
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-    //connect to database
-    // !!!! uncomment to connect to DB
-    // mongoose.connect(config.db.uri, { useNewUrlParser: true });
-    // mongoose.set('useCreateIndex', true);
-    // mongoose.set('useFindAndModify', false);
+  //connect to database
+  // !!!! uncomment to connect to DB
+  // mongoose.connect(config.db.uri, { useNewUrlParser: true });
+  // mongoose.set('useCreateIndex', true);
+  // mongoose.set('useFindAndModify', false);
 
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`worker ${worker.process.pid} died`);
-    });
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
 } else {
-    // instantiating server instance on each workers
-    // servers share the same port though, so we are good
-    var app = require('./server/config/app');
-    var serverWorker = app.start();
+  // instantiating server instance on each workers
+  // servers share the same port though, so we are good
+  var app = require("./server/config/app");
+  app.start();
 
-    console.log(`Worker ${process.pid} started`);
+  console.log(`Worker ${process.pid} started`);
 }
--- server/routes/calculation.server.routes.js
+++ server/routes/calculation.server.routes.js
@@ -1,16 +1,9 @@
 var express = require("express"),
   controller = require("../controllers/calculation.server.controller.js"),
-  cors_handler = require("../config/cors.handler.js");
   router = express.Router();
 
-router
-  .route("/fire")
-  .post(controller.calculateFire, cors_handler.CORS_respond)
-  .options(cors_handler.CORS_handshake);
+router.route("/fire").post(controller.calculateFire);
 
-router
-  .route("/plume")
-  .post(controller.calculateGeneralPlume, cors_handler.CORS_respond)
-  .options(cors_handler.CORS_handshake);
+router.route("/plume").post(controller.calculateGeneralPlume);
 
 module.exports = router;
