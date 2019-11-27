d5161e46cecec698199d25aa324e81cf82cacc95
amakUfl <a.makarevitch@ufl.edu>
2019-11-26 19:30:31 -0500

    Adding nuclide APIs

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 const mongoose = require('mongoose');
+const config = process.env.NODE_ENV == "PROD" ? {} : require('./server/config/config');
 
+(async () => { 
+    if (cluster.isMaster) {
+        console.log(`Master ${process.pid} is running in ${process.env.NODE_ENV || "LOCAL"} environmet.`);
+        console.log("The following clients are allowed to usi this API: ", process.env.ALLOWED_UI_DOMAINS || config.allowed_ui_domains);
 
-if (cluster.isMaster) {
-    console.log(`Master ${process.pid} is running in ${process.env.NODE_ENV || "LOCAL"} environmet.`);
-    const config = process.env.NODE_ENV == "PROD" ? {} : require('./server/config/config');
-    console.log("The following clients are allowed to usi this API: ", process.env.ALLOWED_UI_DOMAINS || config.allowed_ui_domains);
+        // workers to process clients' requests
+        for (let i = 0; i < numCPUs; i++) {
+            cluster.fork();
+        }
 
-    // workers to process clients' requests
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
+        cluster.on('exit', (worker, code, signal) => {
+            console.log(`worker ${worker.process.pid} died`);
+        });
+    } else {
+        //connect to database
+        await mongoose.connect(process.env.DB_KEY || config.db.uri, { useNewUrlParser: true });
+        await mongoose.set('useCreateIndex', true);
+        await mongoose.set('useFindAndModify', false);
+
+        // instantiating server instance on each workers
+        // servers share the same port though, so we are good
+        var app = require('./server/config/app');
+        var serverWorker = app.start();
+
+        console.log(`Worker ${process.pid} started`);
     }
-
-    //connect to database
-    mongoose.connect(process.env.DB_KEY || config.db.uri, { useNewUrlParser: true });
-    mongoose.set('useCreateIndex', true);
-    mongoose.set('useFindAndModify', false);
-
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`worker ${worker.process.pid} died`);
-    });
-} else {
-    // instantiating server instance on each workers
-    // servers share the same port though, so we are good
-    var app = require('./server/config/app');
-    var serverWorker = app.start();
-
-    console.log(`Worker ${process.pid} started`);
-}
+})();
--- server/routes/nuclideResolver.server.routes.js
+++ server/routes/nuclideResolver.server.routes.js
@@ -1,21 +1,21 @@
 var express = require("express"),
   cors_handler = require("../config/cors.handler.js");
-  jsonToExcelParser = require("../controllers/excelDataParser.server.controller.js");
+  nuclides = require("../controllers/nuclideResolver.server.controller");
   router = express.Router();
 
 router
     .route("/getNuclideList")
-    .get(cors_handler.CORS_respond)
+    .get(nuclides.getNuclideList, cors_handler.CORS_respond)
     .options(cors_handler.CORS_handshake);
 
 router
-    .route("/getNuclidesLungClasses")
-    .get(cors_handler.CORS_respond)
+    .route("/getNuclidesLungClasses/:isotop")
+    .get(nuclides.getNuclidesLungClasses, cors_handler.CORS_respond)
     .options(cors_handler.CORS_handshake);
 
 router
-    .route("/getNuclidesICRPLungClass")
-    .get(cors_handler.CORS_respond)
+    .route("/getNuclidesICRPLungClass/:nuclide")
+    .get(nuclides.getNuclidesICRPLungClass, cors_handler.CORS_respond)
     .options(cors_handler.CORS_handshake);
 
-return router;
+module.exports = router;
