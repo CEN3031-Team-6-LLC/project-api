a3f006bf4a4f86c2e23ec89f415fe0657fa9ed8e
amakUfl <a.makarevitch@ufl.edu>
2019-12-02 00:01:22 -0500

    Adding more allowed headers

--- server/config/cors.handler.js
+++ server/config/cors.handler.js
@@ -6,7 +6,7 @@ exports.CORS_handshake = function(req, res) {
     res.header("Access-Control-Allow-Origin", process.env.ALLOWED_UI_DOMAINS || config.allowed_ui_domains);
     res.header("Access-Control-Allow-Methods", "POST, GET");
     res.header("Access-Control-Allow-Headers", 
-        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin");
+        "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin, Content-Disposition");
     res.send();
 }
 
