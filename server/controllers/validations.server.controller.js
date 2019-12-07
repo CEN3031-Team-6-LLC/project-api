29d5e4b3a872f2f8789542f3c8a376498e122f9e
amakUfl <a.makarevitch@ufl.edu>
2019-12-06 22:02:13 -0500

    fix for req corruption

--- server/controllers/validations.server.controller.js
+++ server/controllers/validations.server.controller.js
@@ -30,37 +30,36 @@ exports.validateReqestBody = (req, res, next) => {
     var body = req.body;
     try {
         // general validations
-        if (!body.maxDistance || !body.distanceIncrement)
+        if (!req.body.maxDistance || !req.body.distanceIncrement)
             throw new Error("Both maxDistance and distanceIncrement should be provided!");
-        if (!body.unitSystem)
+        if (!req.body.unitSystem)
             throw new Error("Please specify which unit system ('metric' or 'imperial') is used for length units");
         // parsing numerical values
         numericalFields.forEach(field => {
-            body[field] = parseFloat(body[field]+'');
+            req.body[field] = parseFloat(req.body[field]+'');
         });
         // unit validations
-        if ((typeof body.maxDistance != 'number' && isNaN(parseFloat(body.maxDistance))) || (typeof body.distanceIncrement != 'number' && isNaN(parseFloat(body.distanceIncrement))))
+        if ((typeof req.body.maxDistance != 'number' && isNaN(parseFloat(req.body.maxDistance))) || (typeof req.body.distanceIncrement != 'number' && isNaN(parseFloat(req.body.distanceIncrement))))
             throw new Error("Both maxDistance and distanceIncrement should be numbers!");
-        if (body.maxDistance/body.distanceIncrement > 10000)
+        if (req.body.maxDistance/req.body.distanceIncrement > 10000)
             throw new Error("Either maxDistance is too big or distanceIncrement is too small!\nTechnical limitations start impacting Calculation engine when calculating more than 10000 points.");
-        if (body.maxDistance/body.distanceIncrement < 2)
+        if (req.body.maxDistance/req.body.distanceIncrement < 2)
             throw new Error("Nothing to calculate with the provided maxDistance and distanceIncrement!");
         // required fields checks
         if (req.url.includes("fire")) {
             requiredFireFields.forEach(field => {
-                if (body[field] === undefined || body[field] === null || (typeof body[field] == 'number' && isNaN(body[field])))
+                if (req.body[field] === undefined || req.body[field] === null || (typeof req.body[field] == 'number' && isNaN(req.body[field])))
                     throw new Error(`Missing field "${field}" in the request payload!`);
             });
         } else if (req.url.includes("plume")) {
             requiredPlumeFields.forEach(field => {
-                if (body[field] === undefined || body[field] === null || (typeof body[field] == 'number' && isNaN(body[field])))
+                if (req.body[field] === undefined || req.body[field] === null || (typeof req.body[field] == 'number' && isNaN(req.body[field])))
                     throw new Error(`Missing field "${field}" in the request payload!`);
             });
         }
         // checking if adjustment needed for wind speed
-        if (body.unitSystem == "imperial") body.windSpeed = (body.windSpeed * 22) / 15;
-        // assigning parsed body back to request
-        req.body = body;
+        if (req.body.unitSystem == "imperial") req.body.windSpeed = (req.body.windSpeed * 22) / 15;
+        // assigning parsed req.body back to request
     } catch(err) {
         next(err);
     }
